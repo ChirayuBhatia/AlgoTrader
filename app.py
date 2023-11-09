c7e209db917d7333d4d9c2efb72fe816582c66a1
ChirayuBhatia <120477374+ChirayuBhatia@users.noreply.github.com>
2023-11-10 00:25:27 +0530

    10-11-2023 00:25

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for
 from neo_api_client import NeoAPI
+from flask_socketio import SocketIO
+from datetime import datetime
 
 ex_seg = {"NSE": "nse_cm", "BSE": "bse_cm", "NFO": "nse_fo", "BFO": "bse_fo", "CDS": "cde_fo", "MCX": "mcx_fo"}
 app = Flask(__name__)
+socketio = SocketIO(app)
 client = None
 
 
@@ -23,7 +26,6 @@ def on_close(message):
     print('[OnClose]: ', message)
 
 
-
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -33,6 +35,7 @@ def index():
 def login():
     global client
 
+    mobilenumber = request.form.get('mobilenumber')
     consumer_key = request.form.get('consumer_key')
     consumer_secret = request.form.get('consumer_secret')
     passcode = request.form.get('passcode')
@@ -40,7 +43,7 @@ def login():
 
     # Initialize the NeoAPI client
     client = NeoAPI(consumer_key=consumer_key, consumer_secret=consumer_secret, environment='prod')
-    client.login(mobilenumber="+917987123695", password=passcode)
+    client.login(mobilenumber=f"+91{mobilenumber}", password=passcode)
     client.session_2fa(OTP=mpin)
     # Redirect to a new route after successful login
     return redirect(url_for('dashboard'))
@@ -51,7 +54,7 @@ def dashboard():
     global client
 
     if client:
-        return "Logged in successfully!"
+        return render_template("dashboard.html")
 
     return "Please login first."
 
@@ -59,14 +62,16 @@ def dashboard():
 @app.route('/webhook', methods=["GET", "POST"])
 def webhook():
     global client, ex_seg
-    print(client)
     if client and request.method == 'POST':
         dictionary = request.json
+        socketio.emit('new_log', f"{datetime.now().strftime('%H:%M:%S')} - "
+                                 f'{"Bought" if dictionary["transaction_type"] == "buy" else "Sold"} '
+                                 f'{dictionary["quantity"]} Quantity of {dictionary["trading_symbol"]} '
+                                 f'@ {dictionary["price"]}')
         return client.place_order(exchange_segment=ex_seg[dictionary["exchange_segment"]], product="NRML",
                                   order_type="MKT", quantity=dictionary["quantity"], validity="DAY",
                                   trading_symbol=f"{dictionary['trading_symbol']}-EQ",
                                   transaction_type="B" if dictionary["transaction_type"] == 'buy' else "S")
-
     else:
         return redirect(url_for("index"))
 
